8cc02d4294493256fbc275d55d746441f6b3702a
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2024-11-13 20:45:30 +0100

    Exercici 6 fet

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,18 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+items = {
+    1: {"id": 1, "name": "xavi", "age": 19, "gender": "male", "weight": 68.5, "height": 1.66},
+}
 
 class Item(BaseModel):
     id: int
     name: str
     age: int
+    gender: Optional[str] = None
+    weight: float
+    height: float
     
 @app.get("/items/{item_id}")
-def get_item(item_id: int):
+def get_item(item_id: int, response: Response):
+    if item_id not in items:
+        response.status_code = 404
+        return("ERROR: Item not found ")
     return {"Item id": item_id}
 
 @app.post("/items/")
 def create_item(item: Item):
     
-    return {"Item id": item.id, "Item name": item.name, "Item age": item.age}
+    return {
+        "Item id": item.id,
+        "Item name": item.name,
+        "Item age": item.age,
+        "Description": item.gender,
+        "Price": item.weight,
+        "In stock": item.height
+    }
